8e875564e6ef4f4a141a632124767f5c43d53969
Passamon <Proud_passamon@hotmail.co.th>
2023-05-04 14:52:27 +0700

    add reservation

--- cosr-server/controllers/reservations.js
+++ cosr-server/controllers/reservations.js
@@ -1,4 +1,5 @@
 const Reservation = require("../models/Reservation");
+const CoworkingSpace = require("../models/CoworkingSpace");
 
 //@desc Get all reservation
 //@route GET /api/reservation
@@ -8,7 +9,7 @@ exports.getReservations = async (req, res, next) => {
   if (req.user.role !== "admin") {
     query = Reservation.find({ user: req.user.id }).populate({
       path: "coworkingSpace",
-      select: "name address",
+      select: "name address telephoneNumber",
     });
   } else {
     query = Reservation.find().populate({
@@ -28,5 +29,60 @@ exports.getReservations = async (req, res, next) => {
     return res
       .status(500)
       .json({ success: false, message: "Cannot find Reservation" });
+  }
+};
+
+//@desc Get single reservation
+//@route GET /api/reservation/:id
+//@access Public
+exports.getReservation = async (req, res, next) => {
+  try {
+    const reservation = await Reservation.findById(req.params.id).populate({
+      path: "coworkingSpace",
+      select: "name address telephoneNumber",
+    });
+    if (!reservation) {
+      return res.status(404).json({
+        success: false,
+        message: `No reservation with the id of ${req.params.id}`,
+      });
+    }
+    res.status(200).json({ success: true, data: reservation });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Cannot find Reservation" });
+  }
+};
+
+//@desc Add single reservation
+//@route POST /api/coworkingSpaces/:coworkingSpaceId/reservations
+//@access Private
+exports.addReservation = async (req, res, next) => {
+  try {
+    req.body.coworkingSpace = req.params.coworkingSpaceId;
+    const coworkingSpace = await CoworkingSpace.findById(
+      req.params.coworkingSpaceId
+    );
+    console.log(coworkingSpace);
+    if (!coworkingSpace) {
+      return res.status(404).json({
+        success: false,
+        message: `No coworkingSpace with the id of ${req.params.coworkingSpaceId}`,
+      });
+    }
+
+    const reservation = await Reservation.create(req.body);
+    res.status(200).json({
+      success: true,
+      data: reservation,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Cannot create Reservation",
+    });
   }
 };
--- cosr-server/routes/reservations.js
+++ cosr-server/routes/reservations.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const { getReservations } = require("../controllers/reservations");
+const {
+  getReservations,
+  getReservation,
+  addReservation,
+} = require("../controllers/reservations");
 
 const router = express.Router({ mergeParams: true });
 const { protect } = require("../middleware/auth");
 
-router.route("/").get(protect, getReservations);
+router.route("/").get(protect, getReservations).post(addReservation);
+router.route("/:id").get(getReservation);
 
 module.exports = router;
